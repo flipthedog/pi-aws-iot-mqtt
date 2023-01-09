c7df9fec2592e297bb337b3f3f6fe8714743ba82
Floris <florisvanrossum@gmail.com>
2023-01-09 13:47:05 -0500

    first commit

---
+++ AWSMQTTConnector.py
@@ -0,0 +1,144 @@
+from awscrt import mqtt, auth
+from awsiot import mqtt_connection_builder
+
+import sys
+import threading
+import time
+from uuid import uuid4
+import json
+
+import os, sys
+import numpy as np
+
+# from aws_helper.py @ https://github.com/aws/aws-iot-device-sdk-python-v2
+
+class AWSMQTTConnector():
+
+    def __init__(self,
+        endpoint,
+        ca_file = "root-CA.crt",
+        cert = "pi.cert.pem",
+        key = "pi.private.key",
+        client_id = "basicPubSub",
+        topic = "home/logger/atm_data",
+        region = "us-east-2",
+        count = 0):
+
+        current_cwd = os.getcwd()
+
+        self.endpoint = endpoint
+        self.ca_file = current_cwd + "/auth/" + ca_file
+        self.cert = current_cwd + "/auth/" + cert
+        self.key = current_cwd + "/auth/" + key
+        self.client_id = client_id
+        self.topic = topic
+        self.region = region
+        self.sent_count = count
+        self.received_count = 0
+
+        credentials_provider = auth.AwsCredentialsProvider.new_default_chain()
+
+        self.received_all_event = threading.Event()
+
+        self.mqtt_connection = mqtt_connection_builder.mtls_from_path(
+            endpoint=endpoint,
+            port=443,
+            pri_key_filepath=self.key,
+            cert_filepath=self.cert,
+            ca_filepath=self.ca_file,
+            on_connection_interrupted=self.on_connection_interrupted,
+            on_connection_resumed=self.on_connection_resumed,
+            client_id=client_id,
+            clean_session=False,
+            keep_alive_secs=30,)
+        
+        self.connect_future = self.mqtt_connection.connect()
+
+        try:
+            self.connect_future.result()
+        except e:
+            print("Connection failed")
+            print(e)
+
+    def on_connection_interrupted(self, connection, error, **kwargs):
+        print("Connection interrupted. error: {}".format(error))
+
+    # Callback when an interrupted connection is re-established.
+    def on_connection_resumed(self, connection, return_code, session_present, **kwargs):
+        print("Connection resumed. return_code: {} session_present: {}".format(
+            return_code, session_present))
+
+        if return_code == mqtt.ConnectReturnCode.ACCEPTED and not session_present:
+            print("Session did not persist. Resubscribing to existing topics...")
+            resubscribe_future, _ = connection.resubscribe_existing_topics()
+
+            # Cannot synchronously wait for resubscribe result because we're on the connection's event-loop thread,
+            # evaluate result with a callback instead.
+            resubscribe_future.add_done_callback(on_resubscribe_complete)
+    
+    def on_resubscribe_complete(self, resubscribe_future):
+        resubscribe_results = resubscribe_future.result()
+        print("Resubscribe results: {}".format(resubscribe_results))
+
+        for topic, qos in resubscribe_results['topics']:
+            if qos is None:
+                sys.exit("Server rejected resubscribe to topic: {}".format(topic))
+    
+    # Callback when the subscribed topic receives a message
+    def on_message_received(self, topic, payload, dup, qos, retain, **kwargs):
+        print("Received message from topic '{}': {}".format(topic, payload))
+
+        self.received_count += 1
+        self.received_all_event.set()
+
+    
+    def publish_message(self, msg, topic=None):
+        
+        if topic is None:
+            topic = self.topic
+        
+        message_json = json.dumps(msg)
+
+        print("Publishing Message", message_json)
+
+        self.mqtt_connection.publish(
+            topic=topic,
+            payload=message_json,
+            qos=mqtt.QoS.AT_LEAST_ONCE)
+
+        # self.received_all_event.wait()
+
+        # print("{} message(s) received.".format(
+        #     self.received_count))
+
+    def start_debug_loop(self):
+
+        count = 0
+
+        while True:
+
+            count += 1
+
+            self.publish_message(dict(id=count,
+                                 datetime="22-10-27 00:59:25",
+                                 IAQ=2,
+                                 temperature=3,
+                                 humidity=4,
+                                 pressure=5,
+                                 resistance=6,
+                                 eCO2=7,
+                                 VOC=8,
+                                 CPUt=9))
+
+            print(f"Count: {count}")
+            time.sleep(1)
+        
+    def __exit__(self):
+        disconnect_future = self.mqtt_connection.disconnect()
+        disconnect_future.result()
+    
+if __name__ == '__main__':
+
+    # debug
+    conn = AWSMQTTConnector()
+    conn.start_debug_loop()
---
+++ BSEC.py
@@ -0,0 +1,130 @@
+# Interfaces with bme68x Python C extension
+
+# BSEC Imports 
+from bme68x import BME68X
+import bme68xConstants as cst
+import bsecConstants as bsec
+
+# AWS Connector imports
+from AWSMQTTConnector import AWSMQTTConnector
+
+from datetime import datetime
+import yaml
+import time
+
+from threading import Timer
+
+class BSEC:
+
+    def __init__(self) -> None:
+        
+        # Load YAML config file
+        with open("aws_details.conf", mode="rt", encoding="utf-8") as file:
+            self.aws_config = yaml.safe_load(file)
+
+        self.aws_con = AWSMQTTConnector(self.aws_config["endpoint"])
+
+        self.bme = BME68X(cst.BME68X_I2C_ADDR_HIGH, 1)
+        # self.bme.set_heatr_conf(cst.BME68X_FORCED_MODE, 320, 100, cst.BME68X_ENABLE)
+        self.bme.set_sample_rate(bsec.BSEC_SAMPLE_RATE_LP)
+
+        time.sleep(3)
+
+        self.t = RepeatTimer(5, self.publish_loop)
+        self.t.start()
+
+    def get_data(self):
+        
+        data = None
+        
+        timeout = 3
+        starttime = time.time()
+        endtime = starttime
+
+        while (data is None or data == {}) and (abs(endtime - starttime) < timeout):
+            try:
+                data = self.bme.get_bsec_data()
+                # print(data)
+            except Exception as e:
+                print(e)
+                return None
+
+            time.sleep(0.1)
+
+        return data
+    
+    def publish_to_aws(self, message):
+        
+        # publish_time
+        # sample_nr
+        # timestamp
+        # iaq
+        # iaq_accuracy
+        # static_iaq
+        # static_iaq_accuracy
+        # co2_equivalent
+        # breath_voc_equivalent
+        # breath_voc_accuracy
+        # raw_temperature
+        # raw_pressure
+        # raw_humidity
+        # raw_gas
+        # stabilization_status
+        # run_in_status
+        # temperature
+        # humidity
+        # gas_percentage
+        # gas_percentage_accuracy
+
+        message['publish_time'] = str(datetime.now())
+
+        self.aws_con.publish_message(message, topic = "home/logger/atm_data")
+
+    def publish_loop(self):
+
+        timeout = 3
+
+        starttime = time.time()
+        endtime = starttime
+
+        data = self.get_data()
+        print(data)
+        
+        if data is not None and data != {}:
+            self.publish_to_aws(data)
+        else:
+            print("didn't get data")
+            raise Exception
+
+    def debug_loop(self):
+        
+        while True:
+            # {'sample_nr': 1, 'timestamp': 14984668289437, 'iaq': 50.0, 'iaq_accuracy': 0, 'static_iaq': 50.0, 'static_iaq_accuracy': 0, 
+            # 'co2_equivalent': 600.0, 'co2_accuracy': 0, 'breath_voc_equivalent': 0.49999991059303284, 'breath_voc_accuracy': 0, 
+            # 'raw_temperature': 13.764491081237793, 'raw_pressure': 100982.2421875, 'raw_humidity': 53.53776550292969, 
+            # 'raw_gas': 9319.029296875, 'stabilization_status': 1, 'run_in_status': 1, 'temperature': 8.764491081237793, 
+            # 'humidity': 74.54549407958984, 'gas_percentage': 0.0, 'gas_percentage_accuracy': 0}
+            
+            data = self.get_data()
+
+            # data_dict = json.loads(data)
+            if data is not None and data != {}:
+                print(data)
+                time.sleep(3)
+            else:
+                time.sleep(0.1)
+
+    def __exit__(self):
+        self.aws_con.__exit__()
+        self.t.cancel()
+
+class RepeatTimer(Timer):
+    def run(self):
+        while not self.finished.wait(self.interval):
+            self.function(*self.args, **self.kwargs)
+
+if __name__ == '__main__':
+
+    # debug
+    bsec = BSEC()
+    # bsec.debug_loop()
